eefa6c1856a780b36bebfe0dbe220190a2442cab
Stijn Bannink <SW.Bannink@student.han.nl>
2024-05-22 08:46:56 +0200

    Added GPS localize function

--- version_increment_pre.py
+++ version_increment_pre.py
@@ -29,48 +29,36 @@ VERSION_HEADER = 'Version.h'
 VERSION_PREFIX = '0.1.'
 VERSION_PATCH_NUMBER = 0
 
-if not os.path.exists(".version_no_increment"):
-    try:
-        with open(VERSION_FILE) as FILE:
-            VERSION_PATCH_NUMBER = FILE.readline()
-            VERSION_PREFIX = VERSION_PATCH_NUMBER[0:VERSION_PATCH_NUMBER.rindex('.')+1]
-            VERSION_PATCH_NUMBER = int(VERSION_PATCH_NUMBER[VERSION_PATCH_NUMBER.rindex('.')+1:])
-            if not os.path.exists(".version_no_increment_update_date"):
-                VERSION_PATCH_NUMBER = VERSION_PATCH_NUMBER + 1
-    except:
-        print('No version file found or incorrect data in it. Starting from 0.1.0')
-        VERSION_PATCH_NUMBER = 0
-    with open(VERSION_FILE, 'w+') as FILE:
-        FILE.write(VERSION_PREFIX + str(VERSION_PATCH_NUMBER))
-        print('Build number: {}'.format(VERSION_PREFIX + str(VERSION_PATCH_NUMBER)))
+try:
+    with open(VERSION_FILE) as FILE:
+        VERSION_PATCH_NUMBER = FILE.readline()
+        VERSION_PREFIX = VERSION_PATCH_NUMBER[0:VERSION_PATCH_NUMBER.rindex('.') + 1]
+        VERSION_PATCH_NUMBER = int(VERSION_PATCH_NUMBER[VERSION_PATCH_NUMBER.rindex('.') + 1:])
+except:
+    print('No version file found or incorrect data in it. Starting from 0.1.0')
+    VERSION_PATCH_NUMBER = 0
+with open(VERSION_FILE, 'w+') as FILE:
+    FILE.write(VERSION_PREFIX + str(VERSION_PATCH_NUMBER))
+    print('Build number: {}'.format(VERSION_PREFIX + str(VERSION_PATCH_NUMBER)))
 
-    HEADER_FILE = """
-    // AUTO GENERATED FILE, DO NOT EDIT
-    #ifndef VERSION
-        #define VERSION "{}"
-    #endif
-    #ifndef BUILD_TIMESTAMP
-        #define BUILD_TIMESTAMP "{}"
-    #endif
-    """.format(VERSION_PREFIX + str(VERSION_PATCH_NUMBER), datetime.datetime.now())
+HEADER_FILE = """
+// AUTO GENERATED FILE, DO NOT EDIT
+#ifndef VERSION
+    #define VERSION "{}"
+#endif
+#ifndef BUILD_TIMESTAMP
+    #define BUILD_TIMESTAMP "{}"
+#endif
+""".format(VERSION_PREFIX + str(VERSION_PATCH_NUMBER), datetime.datetime.now())
 
-    if os.environ.get('PLATFORMIO_INCLUDE_DIR') is not None:
-        VERSION_HEADER = os.environ.get('PLATFORMIO_INCLUDE_DIR') + os.sep + VERSION_HEADER
-    elif os.path.exists("include"):
-        VERSION_HEADER = "include" + os.sep + VERSION_HEADER
-    else:
-        PROJECT_DIR = env.subst("$PROJECT_DIR")
-        os.mkdir(PROJECT_DIR + os.sep + "include")
-        VERSION_HEADER = "include" + os.sep + VERSION_HEADER
-
-    with open(VERSION_HEADER, 'w+') as FILE:
-        FILE.write(HEADER_FILE)
-
-    open('.version_no_increment', 'a').close()
+if os.environ.get('PLATFORMIO_INCLUDE_DIR') is not None:
+    VERSION_HEADER = os.environ.get('PLATFORMIO_INCLUDE_DIR') + os.sep + VERSION_HEADER
+elif os.path.exists("include"):
+    VERSION_HEADER = "include" + os.sep + VERSION_HEADER
 else:
-    if os.path.exists("version"):
-        FILE = open(VERSION_FILE)
-        VERSION_NUMBER = FILE.readline()
-        print('Build number: {} (waiting for upload before next increment)'.format(str(VERSION_NUMBER)))
-    else:
-        print('No version file found or incorrect data in it!!')
+    PROJECT_DIR = env.subst("$PROJECT_DIR")
+    os.mkdir(PROJECT_DIR + os.sep + "include")
+    VERSION_HEADER = "include" + os.sep + VERSION_HEADER
+
+with open(VERSION_HEADER, 'w+') as FILE:
+    FILE.write(HEADER_FILE)
